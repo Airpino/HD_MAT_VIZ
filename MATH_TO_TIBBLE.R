46c983bbeae6af5c55c576c35a2a5dc51d81265e
Airpino <antonio.irpino@gmail.com>
2022-09-27 15:13:00 +0200

    fouth commit

--- MATH_TO_TIBBLE.R
+++ MATH_TO_TIBBLE.R
@@ -31,7 +31,7 @@ MATH2tibble<-function(data){
 }
 
 
-one_density<-function(A,v=1,poi=1000,pp=200, n=1024){
+one_density<-function(A,v=1,poi=1000,pp=200, n=1024,NORM=TRUE){
   A<-A %>% select(all_of(v))
   tmp<-unnest(A,cols = 1)
   ran<-range(tmp$x)
@@ -53,14 +53,41 @@ one_density<-function(A,v=1,poi=1000,pp=200, n=1024){
     # 
   }
   #browser()
+  if(NORM) my_y<-sqrt(my_y/max(my_y))
   df0<-data.frame(x=my_x,y=my_y,ID=factor(my_id,levels = as.character(c(1:nr))))
   return(df0)
 }
-
-mul_density<-function(A,v=c(1,2),poi=1000,pp=200, n=256){
+one_density_freq<-function(A,v=1,poi=1000,pp=200, n=1024){
+  A<-A %>% select(all_of(v))
+  tmp<-unnest(A,cols = 1)
+  ran<-range(tmp$x)
+  vals <- seq(from=ran[1],to=ran[2],length.out=poi)
+  nr<-nrow(A)
+  my_x<-my_y<-numeric()
+  my_id<-character()
+  for(i in 1:nr){
+    qua<-HistDAWass:::compQ_vect(distributionH(x=A[,1][[i]]$x,
+                                               p=A[,1][[i]]$y),
+                                 vp=c(0:pp)/pp)
+    tmp1<-density(qua,
+                  from=ran[1], to=ran[2],n = n)
+    #browser()
+    my_x<-c(my_x,tmp1$x)
+    my_y<-c(my_y,tmp1$y)
+    my_id<-c(my_id,rep(as.character(i),length(tmp1$x)))
+    # df0<-data.frame(x=c(tmp1$x,vals),
+    #                 y=c(tmp1$y,rep(NA,length(vals))))
+    # 
+  }
+  #browser()
+  if(NORM) my_y<-sqrt(my_y/max(my_y))
+  df0<-data.frame(x=my_x,y=my_y,ID=factor(my_id,levels = as.character(c(1:nr))))
+  return(df0)
+}
+mul_density<-function(A,v=c(1,2),poi=1000,pp=200, n=256,NORM=TRUE){
   A<-A %>% select(all_of(v))
   NVARS<-length(v)
-  DF1<-one_density(A,v=1,poi=poi,pp=pp, n=n)
+  DF1<-one_density(A,v=1,poi=poi,pp=pp, n=n,NORM=NORM)
   DF1<-DF1 %>% mutate(VAR=rep(as.character(v[1]),nrow(DF1)))
   for (j in 2:NVARS){
     DF2<-one_density(A,v=j,poi=poi,pp=pp, n=n)
@@ -71,11 +98,38 @@ mul_density<-function(A,v=c(1,2),poi=1000,pp=200, n=256){
   return(DF1)
 }
 
+plots_strips_distr_2<-function(B,v=c(1,2),n=64,poi=500,pp=200,NORM=TRUE){
+  
+  plots<-list()
+  require(RColorBrewer)
+  
+  col_strip <- brewer.pal(9, "Reds")
+  df2<-mul_density(B,v = v,n=n,poi=poi,pp = pp,NORM=NORM)
+  #browser()
+    plots<-ggplot(df2 ,
+                       aes(x = x, y = 1, fill = y))+
+      geom_tile(show.legend = T)+#show.legend = F)+
+      scale_fill_gradientn(colors = (col_strip))+
+      facet_grid(rows = vars(ID),
+                             cols =vars(VAR)
+      )+
+      theme_few() +
+      theme(
+        panel.spacing.y=unit(0, "lines"),
+        strip.text.y = element_text(size = 7, angle = 0, hjust = 0),
+        axis.text.y = element_blank(),
+        axis.title.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        panel.border = element_blank())
+  
+  return(plots)
+}
+library(ggthemes)
 
 runfollow=F
 if (runfollow){
   library(ggHoriPlot)
-  library(ggthemes)
+  
   df0<-one_density(B,v = 8,n=256)
   cutpoints <- df0  %>% 
     mutate(
@@ -113,21 +167,21 @@ if (runfollow){
   )
 }
 
-plots_strips_distr<-function(B,v=c(1,2),n=64,poi=500,pp=200){
-
+plots_strips_distr<-function(B,v=c(1,2),n=64,poi=500,pp=200,NORM=TRUE){
+  
   plots<-list()
   require(RColorBrewer)
- 
+  
   col_strip <- brewer.pal(9, "Reds")
-  df2<-mul_density(B,v = v,n=n,poi=poi,pp = pp)
+  df2<-mul_density(B,v = v,n=n,poi=poi,pp = pp,NORM=NORM)
   
   for(j in 1:length(v)){
     
     plots[[j]]<-ggplot(df2 %>% filter(VAR==as.character(v[j])),
                        aes(x = x, y = 1, fill = y))+
-      geom_tile()+#show.legend = F)+
+      geom_tile(show.legend = F)+#show.legend = F)+
       scale_fill_gradientn(colors = (col_strip))+
-      facet_grid(rows = vars(ID)#,
+      facet_grid(rows = vars(ID),
                  #            cols =vars(VAR),
                  #            scales = "free_y"
       )+
@@ -142,7 +196,7 @@ plots_strips_distr<-function(B,v=c(1,2),n=64,poi=500,pp=200){
   }
   return(plots)
 }
-
+####  APPLY -----
 RUNME<-F
 if (RUNME){
   library(RColorBrewer)
@@ -188,3 +242,12 @@ if (RUNME){
       panel.border = element_blank())
   #theme_strip
 }
+
+# library(easyGgplot2)
+# B<-MATH2tibble(China_Seas)
+# plo<-plots_strips_distr(B,v=c(5:8),poi = 50,pp = 50,n = 32)
+# show(easyGgplot2::ggplot2.multiplot(plo[[1]]+theme(strip.text.y = element_blank()) ,
+#                                     plo[[2]]+theme(strip.text.y = element_blank()),
+#                                     plo[[3]]+theme(strip.text.y = element_blank()),
+#                                     plo[[4]],cols=4))
+# library(patchwork)
